f0f46dd2c1954792268b91036639ef9df69b0020
nixonkingko <nkko@hawaii.edu>
2018-09-16 04:03:57 -1000

    finish first exercise (testing output)

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,5 +1,5 @@
 
-const testNums = [1, 2, 3, 4];
+const testNums = [10, 20, 30, 40, 50];
 
 function sumFor(nums) {
   let total = 0;
@@ -11,15 +11,14 @@ function sumFor(nums) {
 console.log(sumFor(testNums));
 
 function sumWhile(nums) {
-  let total = 0;
   let i = 0;
+  let total = 0;
   while (i < nums.length) {
     total += nums[i];
     i++;
   }
   return total;
 }
-
 console.log(sumWhile(testNums));
 
 function sumRecursion(nums) {
@@ -28,11 +27,9 @@ function sumRecursion(nums) {
   }
   return nums[0] + sumRecursion(nums.slice(1, nums.length));
 }
-
 console.log(sumRecursion(testNums));
 
 function sumTheSimpleWay(nums) {
   return _.reduce(nums, function (memo, num) { return memo + num; }, 0);
 }
-
 console.log(sumTheSimpleWay(testNums));
